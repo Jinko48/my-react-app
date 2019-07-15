9a5cf93bbde7805d27d133d27de402ad1712f6ea
Jinko48 <erchandl@umich.edu>
2019-07-15 18:47:28 -0400

    Figured out how to do templating

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 //components
-
+import Header from './components/headerComponent/header';
 // includes
 import './Assets/css/default.css';
+
 
 function App() {
   return (
     <div className="App">
-      Test
+
+
+      <Header />
+
+      
     </div>
   );
 }
---
+++ my-app/src/components/headerComponent/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header>
+            Test Header
+        </header>
+    );
+}
+
+export default Header;
